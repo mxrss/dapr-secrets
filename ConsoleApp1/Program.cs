d9d1b88e8e44807b9828470ebbb4bc6cbe434baa
Roth <MRoth@landam.landamerica.net>
2023-04-18 15:14:37 -0700

    Inital commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Dapr.Client;
+
+namespace Demo
+{
+    public class CLI
+    {
+
+        public static void Main(string[] args) {
+            var daprClient = new DaprClientBuilder().Build();
+            var val = daprClient.GetBulkSecretAsync("local-secret-store");
+            while (true)
+            {
+
+                Console.WriteLine("Value from method: " + val.Result);
+                System.Threading.Thread.Sleep(1000);
+                
+            }
+        }
+
+    }
+
+    }
+
+
